920ae033f40ea28714d84632fea89c009142b989
lcesbron <lcesbron@student.42lyon.fr>
2026-02-12 22:09:22 +0100

    CI: added a test on ex01

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -7,10 +7,19 @@ void	funString(std::string& str)
 	str = "replaced";
 }
 
+void	funString2(std::string const& str)
+{
+	std::cout << str << std::endl;
+}
+
 int	main(void)
 {
 	std::string	arr1[3] = {"vouvou", "test", "bonjour"};
 
 	iter(arr1, 3, funString);
 	for (int i = 0; i < 3; i++) {std::cout << arr1[i] << std::endl;}
+
+	std::string const	arr2[3] = {"vouvou", "test", "bonjour"};
+
+	iter(arr2, 3, funString2);
 }
